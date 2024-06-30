006a66cc1c4f2b0d81d4d6baab5cee4bcb24a247
Vidisha Bhagiya <140824909+vidisha2946@users.noreply.github.com>
2024-06-30 09:54:12 +0530

    Add files via upload

---
+++ DATA-STRUCTURE/LAB-6/BankAccount.java
@@ -0,0 +1,33 @@
+
+public class BankAccount {
+    public String accountNumber;
+    public double balance;
+    public String accountHolderName;
+
+    // Constructor
+    public BankAccount(String accountNumber, String accountHolderName, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = initialBalance;
+    }
+
+    // deposit money
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: " + amount);
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    // money
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew: " + amount);
+        } else {
+
+        }
+    }
+}
